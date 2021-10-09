8812c4db6459f91f6450fcd1517e3f99cc66bf53
大谷東彦 <haruhiko@weseek.co.jp>
2021-10-10 00:45:35 +0900

    - 本番用

--- babel.config.js
+++ babel.config.js
@@ -88,12 +88,6 @@ module.exports = function(api) {
         {
           async: false
         }
-      ],
-      isProductionEnv && [
-        'babel-plugin-transform-react-remove-prop-types',
-        {
-          removeImport: true
-        }
       ]
     ].filter(Boolean)
   }
