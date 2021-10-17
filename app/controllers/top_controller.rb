fa6a606e5fa658ab97f30d184ac222c35d4068f6
kota-t <kota-t@weseek.co.jp>
2021-10-17 15:12:50 +0900

    top_controller#index を修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,6 @@
 
 class TopController < ApplicationController
   def index
-    @meals = Meal.includes(dishes: { thumbnail_image_attachment: :blob }).release.limit(10).reverse
+    @meals = Meal.includes(dishes: { thumbnail_image_attachment: :blob }).release.limit(10).order(created_at: :DESC)
   end
 end
