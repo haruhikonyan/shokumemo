f0dedb859cfc603138cc173b074089be947d72c1
堤宏太 <kota-t@weseek.co.jp>
2021-12-15 00:46:32 +0900

    run annotate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :bigint           not null, primary key
+#  description         :text(65535)
+#  display_name        :string(255)      default(""), not null
+#  email               :string(255)
+#  provider            :string(255)
+#  remember_created_at :datetime
+#  uid                 :string(255)
+#  username            :string(255)
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_uid  (uid) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
